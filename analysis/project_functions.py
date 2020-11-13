d403d3b60dc091363ee8cf032674ed35632b483e
keyvankhademi <keyvankhademi@gmail.com>
2020-11-13 03:54:08 -0800

    Task 5: Add top winrate heroes plot

--- analysis/project_functions.py
+++ analysis/project_functions.py
@@ -3,25 +3,27 @@ import seaborn as sns
 
 
 def load_and_process(url_or_path_to_json_file):
-    matched_df = (
+    matches_df = (
         pd.read_json(url_or_path_to_json_file, lines=True)
-            .dropna()
-            .rename(columns={
+        .dropna()
+        .rename(columns={
             'radiant_score': 'radiant_kills',
             'dire_score': 'dire_kills',
         })
     )
 
-    matched_df['radiant_win'] = matched_df['radiant_win'].apply(lambda x: True if x == 1 else False)
+    matches_df['radiant_win'] = matches_df['radiant_win'].apply(lambda x: True if x == 1 else False)
 
     players = []
-    for game_players in matched_df['players']:
+    for _, match in matches_df.iterrows():
+        game_players = match['players']
         for player in game_players:
+            player['win'] = match['radiant_win'] ^ player['isRadiant']
             players.append(player)
 
     players_df = pd.DataFrame(players)
 
-    return matched_df, players_df
+    return matches_df, players_df
 
 
 def duration_plot(matches_df):
@@ -44,5 +46,16 @@ def radiant_kills_plot(matches_df):
     plot = (
         sns.displot(matches_df, x='radiant_kills')
         .set(xlabel='Radiant Kills (#)', ylabel='Number of matches')
+    )
+    return plot
+
+
+def winrate_plot(players_df):
+    data = players_df.groupby('hero_id').mean().sort_values('win')[-10:]['win']*100
+    plot = (
+        sns.barplot(
+            x=data.index,
+            y=data
+        )
     )
     return plot
